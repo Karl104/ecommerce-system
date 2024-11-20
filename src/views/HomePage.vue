0ee27b71a8f291a73ed19d3cecbd943978cee2dc
Karl <karllastimoso454@gmail.com>
2024-11-21 00:41:52 +0800

    recently code the header and with functionality that i click the button will go either sign-in or sign-up

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,9 +1,142 @@
 <template>
-  <nav>
-    <div class="logo"><img src="../assets/logo-lazy.png" alt="logo" /></div>
-  </nav>
+  <div class="all">
+    <div class="body-home">
+      <header class="header-home">
+        <a href="#" class="logo">
+          <img src="../assets/logo-lazy.png" alt=" logo" class="logo" />
+        </a>
+
+        <span><strong>The not Today Cafe</strong></span>
+
+        <ul class="navbar">
+          <li><a href="#">Menu</a></li>
+          <li><a href="#">About</a></li>
+        </ul>
+
+        <div class="main">
+          <!-- <a href="#" class="user" @click="goToLogin">Sign In</a> -->
+          <button class="signIn" @click="goToLogin">Sign In</button>
+          <button class="signUp" @click="goToSignUp">Sign Up</button>
+
+          <a href="#" class="icon">
+            <img src="../assets/cart.png" alt="cart" />
+          </a>
+        </div>
+      </header>
+    </div>
+  </div>
 </template>
 
-<script></script>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+.all {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  text-decoration: none;
+  list-style: none;
+  font-family: 'Kanit', sans-serif;
+}
 
-<style></style>
+:root {
+  --bg-color: #ffffff;
+  --text-color: #000000;
+  --main-color: #828282;
+}
+
+.body-home {
+  min-height: 100vh;
+  background: var(--bg-color);
+  color: var(--text-color);
+}
+
+.header-home {
+  position: fixed;
+  width: 100%;
+  top: 0;
+  right: 0;
+  z-index: 1000;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px 5%;
+  background-color: #ffffff;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+  /* transition: all 0.5s ease; */
+}
+.logo {
+  display: flex;
+  width: 130px;
+  align-items: center;
+  height: 130px;
+}
+
+span {
+  font-size: 25px;
+  font-weight: 600;
+  margin-right: 6px;
+}
+.navbar {
+  display: flex;
+}
+
+.navbar a {
+  color: var(--text-color);
+  font-size: 1.1rem;
+  font-weight: 500;
+  padding: 5px 0;
+  margin: 0px 30px;
+  text-decoration: none;
+}
+
+.navbar a:hover {
+  color: var(--main-color);
+}
+.main {
+  display: flex;
+  align-items: center;
+}
+
+.main .signIn,
+.main .signUp {
+  margin-right: 15px;
+  padding: 8px 20px;
+  font-size: 1rem;
+  font-weight: 600;
+  color: var(--text-color);
+  border: 2px solid var(--text-color);
+  border-radius: 5px;
+  background-color: transparent;
+  transition:
+    background-color 0.3s,
+    color 0.3s;
+}
+
+.main .signIn:hover,
+.main .signUp:hover {
+  background-color: var(--text-color);
+  color: #ffffff;
+}
+
+li {
+  list-style-type: none;
+}
+
+.icon {
+  margin-right: 30px;
+  margin-left: 30px;
+}
+</style>
+
+<script>
+export default {
+  methods: {
+    goToLogin() {
+      this.$router.push('/login')
+    },
+    goToSignUp() {
+      this.$router.push('/signup')
+    },
+  },
+}
+</script>
