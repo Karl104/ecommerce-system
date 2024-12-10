4711dcbcc7fd2700a6a70ca40ceeed01b75d962b
Karl <karllastimoso454@gmail.com>
2024-12-11 01:26:59 +0800

    added the menu path

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/HomePage.vue'
 import LoginPage from '../views/LoginPage.vue'
 import SignUp from '../components/SignUp.vue'
+import MenuPage from '../views/MenuPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,6 +22,12 @@ const router = createRouter({
       path: '/SignUp',
       name: 'SignUp',
       component: SignUp,
+    },
+
+    {
+      path: '/menu',
+      name: 'menu',
+      component: MenuPage,
     },
   ],
 })
